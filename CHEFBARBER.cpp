8af983c44c3820fafc19fd1fa23c9070d3ef56e6
Ashish Yadav <ay079396@gmail.com>
2022-02-04 16:12:57 +0530

    Feb long challenge

---
+++ CHEFBARBER.cpp
@@ -0,0 +1,16 @@
+// problem link -https://www.codechef.com/FEB221C/problems/CHEFBARBER
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m,ans;
+	    cin>>n>>m<<endl;
+		ans=n*m;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
