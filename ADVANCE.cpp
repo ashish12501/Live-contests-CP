7c54f7d3ca5b2daaf89282b6258fecf6d1850a4c
Ashish Yadav <ay079396@gmail.com>
2022-06-08 15:21:47 +0530

    adding ans of last taken contest

---
+++ ADVANCE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int t;  
+   cin>>t;
+   {
+       int x,y;
+       cin>>x>>y;
+       if (y>=x && y<=x+200) cout<<"yes";
+       else cout<<"No";
+   }
+}
